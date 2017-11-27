ed5aa187cbb5087dbc88e26236add4c0bb4a3fc2
sebgrebe <sebbecker@gmx.net>
2017-11-27 21:22:54 +0000

    db heroic

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url': 'mongodb://localhost:27017/voting'
+	'url': 'mongodb://heroku_jgx3qw08:8eqiri8mkbpq6d4d26ku979dnm@ds123146.mlab.com:23146/heroku_jgx3qw08' || 'mongodb://localhost:27017/voting'
 }
