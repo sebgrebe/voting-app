380c469e73afefcfaa56acae9960b427a2a265e6
sebgrebe <sebbecker@gmx.net>
2017-11-28 17:53:00 +0000

    undo changes

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url': 'mongodb://heroku_jgx3qw08:8eqiri8mkbpq6d4d26ku979dnm@ds123146.mlab.com:23146/heroku_jgx3qw08' || 'mongodb://localhost:27017/voting'
+	'url': 'mongodb://localhost:27017/voting' || 'mongodb://heroku_jgx3qw08:8eqiri8mkbpq6d4d26ku979dnm@ds123146.mlab.com:23146/heroku_jgx3qw08'
 }
