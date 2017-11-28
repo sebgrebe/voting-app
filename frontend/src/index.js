f49e025409a09444c7107deb4e8f29b417dd1d82
sebgrebe <sebbecker@gmx.net>
2017-11-28 21:12:17 +0000

    hashHistory

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory, Router, Route } from 'react-router';
+import { hashHistory, Router, Route } from 'react-router';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
 registerServiceWorker();
 
 ReactDOM.render((
-	<Router history={browserHistory}>
+	<Router history={hashHistory}>
 		<Route path="/" component={App} />
 		<Route path='/polls/*' component={App} poll={true}/>
 	</Router>
