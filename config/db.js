c422bf746cc1c078e0ba6b70d3ba9f7ca7f2d476
sebgrebe <sebbecker@gmx.net>
2017-11-28 18:04:39 +0000

    fix db link

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
+var db_url = 'mongodb://heroku_jgx3qw08:8eqiri8mkbpq6d4d26ku979dnm@ds123146.mlab.com:23146/heroku_jgx3qw08' || 'mongodb://localhost:27017/voting' 
+
 module.exports = {
-	'url': 'mongodb://localhost:27017/voting' || 'mongodb://heroku_jgx3qw08:8eqiri8mkbpq6d4d26ku979dnm@ds123146.mlab.com:23146/heroku_jgx3qw08'
+	'url': db_url
 }
